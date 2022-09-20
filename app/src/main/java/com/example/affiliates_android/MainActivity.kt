54fda750a90620ba9e6a1e1f352d5f1c0939eda0
anna <mink141416@gmail.com>
2022-09-20 22:33:45 +0900

    09.20 add viewBinding, BottomNavigationView

--- app/src/main/java/com/example/affiliates_android/MainActivity.kt
+++ app/src/main/java/com/example/affiliates_android/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.affiliates_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.affiliates_android.databinding.ActivityMainBinding
+import com.example.affiliates_android.ui.view.AddOrDeleteFragment
+import com.example.affiliates_android.ui.view.HomeFragment
+import com.example.affiliates_android.ui.view.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+    }
+
+    // bottomNavigationView
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_add_or_delete -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, AddOrDeleteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
