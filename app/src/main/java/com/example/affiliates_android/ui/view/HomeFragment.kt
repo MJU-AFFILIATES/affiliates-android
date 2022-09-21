98a670c9efd12b668dc6dda13011f1f0583de4b0
anna <mink141416@gmail.com>
2022-09-21 21:30:07 +0900

    09.21 add kakao mapView

--- app/src/main/java/com/example/affiliates_android/ui/view/HomeFragment.kt
+++ app/src/main/java/com/example/affiliates_android/ui/view/HomeFragment.kt
@@ -6,23 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.affiliates_android.databinding.FragmentHomeBinding
+import net.daum.mf.map.api.MapView
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding?= null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val mapView = MapView(context)
+        binding.mapView.addView(mapView)
+
         return binding.root
     }
+
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
+
+
 
 }
