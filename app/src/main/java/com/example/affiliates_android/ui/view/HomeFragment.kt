14ec7c5210c87c93320760bfc4c97473d58763fa
anna <mink141416@gmail.com>
2022-09-29 10:31:11 +0900

    [FEAT] Home layout

--- app/src/main/java/com/example/affiliates_android/ui/view/HomeFragment.kt
+++ app/src/main/java/com/example/affiliates_android/ui/view/HomeFragment.kt
@@ -11,7 +11,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.affiliates_android.data.repository.UserRepositoryImpl
 import com.example.affiliates_android.databinding.ActivityMainBinding
-import com.example.affiliates_android.databinding.FragmentHomeBinding
+//import com.example.affiliates_android.databinding.FragmentHomeBinding
 import com.example.affiliates_android.ui.viewmodel.UserViewModel
 import com.example.affiliates_android.ui.viewmodel.UserViewModelProviderFactory
 import com.google.android.gms.maps.model.LatLng
@@ -27,34 +27,34 @@ import java.util.*
 
 
 class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
-    val binding get() = _binding!!
-
-    // viewModel 초기화
-    lateinit var userViewModel: UserViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        // viewModel 사용
-        val userRepository = UserRepositoryImpl()
-        val factory = UserViewModelProviderFactory(userRepository)
-        userViewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
-
-//        val mapView = MapView(context)
-//        binding.mapView.addView(mapView)
-
-        return binding.root
-    }
-
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
-    }
+//    private var _binding: FragmentHomeBinding? = null
+//    val binding get() = _binding!!
+//
+//    // viewModel 초기화
+//    lateinit var userViewModel: UserViewModel
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?,
+//    ): View? {
+////        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+//
+//        // viewModel 사용
+//        val userRepository = UserRepositoryImpl()
+//        val factory = UserViewModelProviderFactory(userRepository)
+//        userViewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+//
+////        val mapView = MapView(context)
+////        binding.mapView.addView(mapView)
+//
+//        return binding.root
+//    }
+//
+//
+//    override fun onDestroyView() {
+//        _binding = null
+//        super.onDestroyView()
+//    }
 
 }
