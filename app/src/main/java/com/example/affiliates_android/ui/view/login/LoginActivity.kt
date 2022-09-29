13e4fe057da569d2a08e57c098eb9d86d274a528
wkdyujin <yujin1203713@gmail.com>
2022-09-29 18:48:52 +0900

    [FEAT] 로그인 뷰 완료

---
+++ app/src/main/java/com/example/affiliates_android/ui/view/login/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.affiliates_android.ui.view.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.affiliates_android.databinding.ActivityLoginBinding
+import com.example.affiliates_android.ui.view.MainActivity
+
+class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        
+        binding.testBtn.setOnClickListener{
+            // 메인 액티비티로 전환
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.loginSignupBtn.setOnClickListener {
+            startActivity(Intent(this, ActivitySignUp::class.java))
+        }
+    }
+}
