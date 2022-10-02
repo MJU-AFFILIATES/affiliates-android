68a9ef5fe25c3490d8adbed3ccd3cff16588c9de
anna <mink141416@gmail.com>
2022-10-02 12:44:19 +0900

    [FEAT] 명지대학교 중심

--- app/src/main/java/com/example/Affiliates/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/Affiliates/ui/view/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.Affiliates.ui.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.Affiliates.R
 import com.example.Affiliates.data.repository.UserRepositoryImpl
 import com.example.Affiliates.databinding.ActivityMainBinding
 import com.example.Affiliates.ui.viewmodel.UserViewModelProviderFactory
+import com.google.android.gms.maps.MapFragment
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMapSdk
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickLis
         binding.mapView
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -39,23 +44,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickLis
         val factory = UserViewModelProviderFactory(userRepository)
 //        userViewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
 
-//        binding.settingIv.setOnClickListener{
-//            startActivity(Intent(this, SettingActivity::class.java))
-//        }
+        binding.settingIv.setOnClickListener{
+            startActivity(Intent(this, SettingActivity::class.java))
+        }
+
 
     }
 
     override fun onMapReady(map: NaverMap) {
         naverMap = map
 
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.580153641583045, 126.9228129200726))
+        naverMap.moveCamera(cameraUpdate)
+
         val uiSetting = naverMap.uiSettings
         uiSetting.isLocationButtonEnabled = false
+
 
     }
 
     override fun onClick(p0: Overlay): Boolean {
         TODO("Not yet implemented")
     }
+
+
 
 
 }
